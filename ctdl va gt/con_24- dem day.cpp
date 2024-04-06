bc955710b022e36cc2194f7723018ae0085e5b27
nguyensonptit <162606458+nguyensonptit@users.noreply.github.com>
2024-04-06 14:53:14 +0700

    Create con_24- dem day.cpp

---
+++ ctdl va gt/con_24- dem day.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+const long long m = 123456789;
+
+long long powmod(long long a, long long b){
+	if(b == 0)  return 1;
+	if(b == 1)  return a;
+	long long tmp = powmod(a, b /2);
+	if(b % 2){
+		return (((tmp * tmp) % m) * a) % m;
+	}
+	return (tmp * tmp) % m;
+}
+
+void kt(){
+	long long n;
+	cin>>n;
+	cout<<powmod(2, n - 1)<<endl;
+}
+int main(){
+	int t=1;
+	cin>>t;
+	while(t--){
+		kt();
+	}
+	return 0;
+}
